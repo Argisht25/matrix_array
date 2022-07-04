f77e876ad34e865f0db7f376afd70c0b6cea3dbe
Argisht <argoo17@mail.ru>
2022-07-04 17:40:24 +0400

    matrix array

---
+++ matrix_element.js
@@ -0,0 +1,21 @@
+//[0, 0, 1, 0, 0],
+//[2, 1, 3, 3, 5],
+//[1, 2, 3, 4],
+//[1, 2, 3, 4]
+function matrixElementsSum(matrix) {
+    debugger
+    let sum = 0;
+    for (let i = 0; i < matrix.length; i++) {
+        const currRow = matrix[i];
+        for (let col = 0; col < currRow.length; col++) {
+            if (currRow[col] === 0) {
+                if (i + 1 < matrix.length) {
+                    matrix[i + 1][col] = 0;
+                }
+            } else {
+                sum += currRow[col];
+            }
+        }
+    }
+    return sum;
+}
